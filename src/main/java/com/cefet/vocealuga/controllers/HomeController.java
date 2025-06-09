30c2399a25f356f11ffbe2d07694a3221b945ade
Luiz Reis <reisluiz05@gmail.com>
2025-06-09 20:55:15 -0300

    creating system layout

--- src/main/java/com/cefet/vocealuga/controllers/HomeController.java
+++ src/main/java/com/cefet/vocealuga/controllers/HomeController.java
@@ -1,14 +1,28 @@
 package com.cefet.vocealuga.controllers;
 
+import com.cefet.vocealuga.models.Usuario;
+import com.cefet.vocealuga.services.UsuarioService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
 
+    private final UsuarioService usuarioService;
+
+    public HomeController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
     @GetMapping("/home")
-    public String home() {
-        return "home";
+    public String home(Model model, Principal principal) {
+        Usuario usuario = usuarioService.usuarioLogado();
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("conteudo", "home");
+        return "layout";
     }
 
     @GetMapping("/login")
