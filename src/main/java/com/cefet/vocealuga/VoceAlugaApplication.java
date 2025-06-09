7785a8800a419f2a15461b52bb9c361fcf4660ba
Luiz Reis <reisluiz05@gmail.com>
2025-06-09 20:06:02 -0300

    adding flyway and creating first migration

--- src/main/java/com/cefet/vocealuga/VoceAlugaApplication.java
+++ src/main/java/com/cefet/vocealuga/VoceAlugaApplication.java
@@ -2,12 +2,20 @@ package com.cefet.vocealuga;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class VoceAlugaApplication {
+public class VoceAlugaApplication implements  Runnable{
 
     public static void main(String[] args) {
         SpringApplication.run(VoceAlugaApplication.class, args);
     }
 
+    @Override
+    public void run() {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("admin@1234"));
+    }
 }
---
+++ src/main/resources/db/migration/V2__Criando_Dados_Iniciais.sql
@@ -0,0 +1,14 @@
+INSERT INTO endereco (id, logradouro, numero, complemento, bairro, cidade, estado, cep) VALUES
+    (1, 'R. Miguel Ângelo', '96', '', 'Maria da Graça', 'Rio de Janeiro', 'RJ', '20785-220');
+
+INSERT INTO empresa (id, razao_social, nome_fantasia, cnpj, telefone, endereco_id) VALUES
+    (1, 'Empresa Aluga Mais LTDA', 'Aluga Mais', '11.111.111/0001-11', '(31) 99999-0000', 1);
+
+INSERT INTO filial (id, nome, telefone, email, endereco_id, empresa_id) VALUES
+    (1, 'Filial Matriz', '(31) 98888-0000', 'matriz@alugamais.com', 1, 1);
+
+INSERT INTO usuario (id, nome, cpf, email, senha, data_de_nascimento, telefone) VALUES
+    (1, 'Luiz Reis', '123.456.789-00', 'luiz@alugamais.com', '$2a$10$aHQGv3L0FpF0yNhxLztKle2K57VO5/3DFhvzYQgGsjZkJjI8KkQd2', '2004-09-20', '(21) 91234-5678');
+
+INSERT INTO operador (id, usuario_id, filial_id, cargo) VALUES
+    (1, 1, 1, 'Administrador');
