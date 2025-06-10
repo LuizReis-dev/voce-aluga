117a1543c8d05bb35544e8d1cdb70b35f66d12ac
Luiz Reis <reisluiz05@gmail.com>
2025-06-09 23:10:55 -0300

    implementing create and edit user

--- src/main/java/com/cefet/vocealuga/controllers/UsuarioController.java
+++ src/main/java/com/cefet/vocealuga/controllers/UsuarioController.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -36,13 +39,31 @@ public class UsuarioController {
     }
 
     @GetMapping("/usuarios/{id}/editar")
-    public String editarUsuario(@PathVariable Long id, Model model) {
+    public String editarUsuario(@PathVariable Integer id, Model model) {
         Usuario usuarioLogado = usuarioService.usuarioLogado();
         model.addAttribute("usuarioLogado", usuarioLogado);
         Usuario usuario = usuarioService.findById(id);
+        System.out.println(usuario.getDataNascimento());
         model.addAttribute("usuario", usuario);
         model.addAttribute("conteudo", "/usuarios/cadastro");
         return "layout";
+    }
+
+    @PostMapping("/usuarios")
+    public String salvarUsuario(Usuario usuario, RedirectAttributes redirectAttributes, @RequestParam(required = false) String senha) {
+        try {
+            usuario.setSenha(senha);
+            usuarioService.salvar(usuario);
+            redirectAttributes.addFlashAttribute("success", "Usuário salvo com sucesso!");
+            return "redirect:/usuarios";
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", "Erro ao salvar o usuário: " + e.getMessage());
+            if(usuario.getId() != null) {
+                return "redirect:/usuarios/" +usuario.getId()+"/editar";
+            }
+
+            return "redirect:/usuarios/cadastro";
+        }
     }
 
 }
--- src/main/java/com/cefet/vocealuga/services/UsuarioService.java
+++ src/main/java/com/cefet/vocealuga/services/UsuarioService.java
@@ -1,13 +1,18 @@
 package com.cefet.vocealuga.services;
 
+import com.cefet.vocealuga.models.Operador;
 import com.cefet.vocealuga.models.Usuario;
 import com.cefet.vocealuga.repositories.UsuarioRepository;
+import com.cefet.vocealuga.utils.CPFValidator;
+import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,9 +21,11 @@ import java.util.List;
 public class UsuarioService implements UserDetailsService {
 
     private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UsuarioService(UsuarioRepository usuarioRepository) {
+    public UsuarioService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
         this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -28,18 +35,69 @@ public class UsuarioService implements UserDetailsService {
     }
 
     public Usuario usuarioLogado() {
-        return  (Usuario) SecurityContextHolder
+        var usuario =  (Usuario) SecurityContextHolder
                 .getContext()
                 .getAuthentication()
                 .getPrincipal();
+        return findById(usuario.getId());
     }
 
     public List<Usuario> findAll() {
         return usuarioRepository.findAll();
     }
 
-    public Usuario findById(Long id) {
+    public Usuario findById(Integer id) {
         return usuarioRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
+    }
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        if(usuario.getId() != null) {
+            return editar(usuario);
+        }
+
+        if(usuarioRepository.existsByEmailOrCpf(usuario.getEmail(), usuario.getCpf())) {
+            throw new EntityExistsException("Já existe um usuário com este email ou cpf");
+        }
+
+        if(!CPFValidator.validarCPF(usuario.getCpf())) {
+            throw new RuntimeException("CPF inválido");
+        }
+
+        usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
+
+        Operador operador = new Operador();
+        operador.setCargo("Administrador");
+        operador.setFilial(usuarioLogado().getOperador().getFilial());
+        operador.setUsuario(usuario);
+
+        usuario.setOperador(operador);
+
+        return usuarioRepository.save(usuario);
+    }
+
+    public Usuario editar(Usuario usuario) {
+        Usuario existente = usuarioRepository.findById(usuario.getId())
+                .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
+
+        if (usuarioRepository.existsByEmailOrCpfAndIdNot(usuario.getEmail(), usuario.getCpf(), usuario.getId())) {
+            throw new EntityExistsException("Já existe um usuário com este email ou cpf");
+        }
+
+        if (!CPFValidator.validarCPF(usuario.getCpf())) {
+            throw new RuntimeException("CPF inválido");
+        }
+
+        existente.setNome(usuario.getNome());
+        existente.setEmail(usuario.getEmail());
+        existente.setCpf(usuario.getCpf());
+
+        if (usuario.getSenha() != null && !usuario.getSenha().isBlank() &&
+                !passwordEncoder.matches(usuario.getSenha(), existente.getSenha())) {
+            existente.setSenha(passwordEncoder.encode(usuario.getSenha()));
+        }
+
+        return usuarioRepository.save(existente);
     }
 }
