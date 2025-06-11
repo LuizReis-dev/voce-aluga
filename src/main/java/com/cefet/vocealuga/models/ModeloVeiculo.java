0bc29e48dd3c2bb73318313476205b73ed7bdd21
Luiz Reis <reisluiz05@gmail.com>
2025-06-11 19:40:34 -0300

    creating ModeloVeiculo and Veiculo entities

---
+++ src/main/java/com/cefet/vocealuga/models/ModeloVeiculo.java
@@ -0,0 +1,70 @@
+package com.cefet.vocealuga.models;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "modelo_veiculo")
+public class ModeloVeiculo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String marca;
+    private String modelo;
+    private Integer ano;
+
+    @OneToMany(mappedBy = "modelo")
+    private List<Veiculo> veiculos = new ArrayList<>();
+
+    public ModeloVeiculo() {
+    }
+
+    public ModeloVeiculo(Integer id, String marca, String modelo, Integer ano) {
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    public List<Veiculo> getVeiculos() {
+        return veiculos;
+    }
+
+    public void setVeiculos(List<Veiculo> veiculos) {
+        this.veiculos = veiculos;
+    }
+}
