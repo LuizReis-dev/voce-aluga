5534e629a73aab15ca8321001959a8cd751fe8b5
Luiz Reis <reisluiz05@gmail.com>
2025-06-18 19:06:53 -0300

    mapping user and roles

--- src/main/java/com/cefet/vocealuga/config/SecurityConfig.java
+++ src/main/java/com/cefet/vocealuga/config/SecurityConfig.java
@@ -6,12 +6,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,6 +19,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/admin/**").hasAnyRole("OPERADOR", "ADMINISTRADOR")
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
