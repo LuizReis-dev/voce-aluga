347cb31a3fe934c3ea2b26a0c6545a5a60c46c9e
ecbDeveloper <developer.ecb@gmail.com>
2025-06-20 09:30:21 -0300

    adiciona login e cadastro do cliente

--- src/main/java/com/cefet/vocealuga/models/Usuario.java
+++ src/main/java/com/cefet/vocealuga/models/Usuario.java
@@ -8,142 +8,142 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
 import java.util.List;
 
 @Entity
 @Table(name = "usuario")
 public class Usuario implements UserDetails {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    @Column(nullable = false)
-    private String nome;
-    @Column(nullable = false, unique = true, length = 14)
-    private String cpf;
-    @Column(nullable = false, unique = true)
-    private String email;
-    private String senha;
-    @Column(name = "data_de_nascimento", nullable = false)
-    private LocalDate dataNascimento;
-    @Column(length = 20)
-    private String telefone;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(nullable = false)
+	private String nome;
+	@Column(nullable = false, unique = true, length = 14)
+	private String cpf;
+	@Column(nullable = false, unique = true)
+	private String email;
+	private String senha;
+	@Column(name = "data_de_nascimento", nullable = false)
+	private LocalDate dataNascimento;
+	@Column(length = 20)
+	private String telefone;
 
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "usuario")
-    private Operador operador;
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "usuario")
+	private Operador operador;
 
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "usuario")
-    private Cliente cliente;
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "usuario")
+	private Cliente cliente;
 
-    public Usuario() {
-    }
+	public Usuario() {
+	}
 
-    public Usuario(Integer id, String nome, String cpf, String email, String senha, LocalDate dataNascimento, String telefone) {
-        this.id = id;
-        this.nome = nome;
-        this.cpf = cpf;
-        this.email = email;
-        this.senha = senha;
-        this.dataNascimento = dataNascimento;
-        this.telefone = telefone;
-    }
+	public Usuario(Integer id, String nome, String cpf, String email, String senha, LocalDate dataNascimento,
+			String telefone) {
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.senha = senha;
+		this.dataNascimento = dataNascimento;
+		this.telefone = telefone;
+	}
 
-    public Integer getId() {
-        return id;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public String getCpf() {
-        return cpf;
-    }
+	public String getCpf() {
+		return cpf;
+	}
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getSenha() {
-        return senha;
-    }
+	public String getSenha() {
+		return senha;
+	}
 
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 
-    public LocalDate getDataNascimento() {
-        return dataNascimento;
-    }
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
 
-    public void setDataNascimento(LocalDate dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
 
-    public String getTelefone() {
-        return telefone;
-    }
+	public String getTelefone() {
+		return telefone;
+	}
 
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
 
-    public Operador getOperador() {
-        return operador;
-    }
+	public Operador getOperador() {
+		return operador;
+	}
 
-    public void setOperador(Operador operador) {
-        this.operador = operador;
-    }
+	public void setOperador(Operador operador) {
+		this.operador = operador;
+	}
 
-    public Cliente getCliente() {
-        return cliente;
-    }
+	public Cliente getCliente() {
+		return cliente;
+	}
 
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        List<GrantedAuthority> authorities = new ArrayList<>();
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> authorities = new ArrayList<>();
 
-        if (this.operador != null) {
-            if (CargoOperador.OPERADOR.equals(this.operador.getCargo())) {
-                authorities.add(new SimpleGrantedAuthority("ROLE_OPERADOR"));
-            } else if (CargoOperador.ADMINISTRADOR.equals(this.operador.getCargo())) {
-                authorities.add(new SimpleGrantedAuthority("ROLE_ADMINISTRADOR"));
-            }
-        } else if (this.cliente != null) {
-            authorities.add(new SimpleGrantedAuthority("ROLE_CLIENTE"));
-        }
+		if (this.operador != null) {
+			if (CargoOperador.OPERADOR.equals(this.operador.getCargo())) {
+				authorities.add(new SimpleGrantedAuthority("ROLE_OPERADOR"));
+			} else if (CargoOperador.ADMINISTRADOR.equals(this.operador.getCargo())) {
+				authorities.add(new SimpleGrantedAuthority("ROLE_ADMINISTRADOR"));
+			}
+		} else if (this.cliente != null) {
+			authorities.add(new SimpleGrantedAuthority("ROLE_CLIENTE"));
+		}
 
-        return authorities;
-    }
+		return authorities;
+	}
 
-    @Override
-    public String getPassword() {
-        return this.senha;
-    }
+	@Override
+	public String getPassword() {
+		return this.senha;
+	}
 
-    @Override
-    public String getUsername() {
-        return this.email;
-    }
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
 }
--- src/main/java/com/cefet/vocealuga/services/UsuarioService.java
+++ src/main/java/com/cefet/vocealuga/services/UsuarioService.java
@@ -1,6 +1,7 @@
 package com.cefet.vocealuga.services;
 
 import com.cefet.vocealuga.models.CargoOperador;
+import com.cefet.vocealuga.models.Cliente;
 import com.cefet.vocealuga.models.Operador;
 import com.cefet.vocealuga.models.Usuario;
 import com.cefet.vocealuga.repositories.UsuarioRepository;
@@ -9,7 +10,6 @@ import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -21,97 +21,115 @@ import java.util.List;
 @Service
 public class UsuarioService implements UserDetailsService {
 
-    private static Usuario usuarioLogado;
-    private final UsuarioRepository usuarioRepository;
-    private final PasswordEncoder passwordEncoder;
+	private static Usuario usuarioLogado;
+	private final UsuarioRepository usuarioRepository;
+	private final PasswordEncoder passwordEncoder;
 
-    public UsuarioService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
-        this.usuarioRepository = usuarioRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
+	public UsuarioService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
+		this.usuarioRepository = usuarioRepository;
+		this.passwordEncoder = passwordEncoder;
+	}
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return usuarioRepository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + username));
-    }
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return usuarioRepository.findByEmail(username)
+				.orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + username));
+	}
 
-    public Usuario usuarioLogado() {
-        var usuario = (Usuario) SecurityContextHolder
-                .getContext()
-                .getAuthentication()
-                .getPrincipal();
-        if(usuarioLogado == null || !usuarioLogado.getId().equals(usuario.getId())) {
+	public Usuario usuarioLogado() {
+		var usuario = (Usuario) SecurityContextHolder
+				.getContext()
+				.getAuthentication()
+				.getPrincipal();
+		if (usuarioLogado == null || !usuarioLogado.getId().equals(usuario.getId())) {
 
-            usuarioLogado = findById(usuario.getId());
-        }
-        return usuarioLogado;
-    }
+			usuarioLogado = findById(usuario.getId());
+		}
+		return usuarioLogado;
+	}
 
-    public List<Usuario> findAll() {
-        return usuarioRepository.findAll();
-    }
+	public List<Usuario> findAll() {
+		return usuarioRepository.findAll();
+	}
 
-    public Usuario findById(Integer id) {
-        return usuarioRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
-    }
+	public Usuario findById(Integer id) {
+		return usuarioRepository.findById(id)
+				.orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
+	}
 
-    @Transactional
-    public Usuario salvar(Usuario usuario) {
-        if(usuario.getId() != null) {
-            return editar(usuario);
-        }
+	@Transactional
+	public Usuario salvar(Usuario usuario) {
+		if (usuario.getId() != null) {
+			return editar(usuario);
+		}
 
-        if(usuarioRepository.existsByEmailOrCpf(usuario.getEmail(), usuario.getCpf())) {
-            throw new EntityExistsException("Já existe um usuário com este email ou cpf");
-        }
+		if (usuarioRepository.existsByEmailOrCpf(usuario.getEmail(), usuario.getCpf())) {
+			throw new EntityExistsException("Já existe um usuário com este email ou cpf");
+		}
 
-        if(!CPFValidator.validarCPF(usuario.getCpf())) {
-            throw new RuntimeException("CPF inválido");
-        }
+		if (!CPFValidator.validarCPF(usuario.getCpf())) {
+			throw new RuntimeException("CPF inválido");
+		}
 
-        usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
+		usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
 
-        Operador operador = new Operador();
-        operador.setCargo(CargoOperador.OPERADOR);
-        operador.setFilial(usuarioLogado().getOperador().getFilial());
-        operador.setUsuario(usuario);
+		Operador operador = new Operador();
+		operador.setCargo(CargoOperador.OPERADOR);
+		operador.setFilial(usuarioLogado().getOperador().getFilial());
+		operador.setUsuario(usuario);
 
-        usuario.setOperador(operador);
+		usuario.setOperador(operador);
 
-        return usuarioRepository.save(usuario);
-    }
+		return usuarioRepository.save(usuario);
+	}
 
-    public Usuario editar(Usuario usuario) {
-        Usuario existente = usuarioRepository.findById(usuario.getId())
-                .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
+	public Usuario salvarCliente(Usuario usuario) {
+		if (usuarioRepository.existsByEmailOrCpf(usuario.getEmail(), usuario.getCpf())) {
+			throw new EntityExistsException("Já existe um usuário com este email ou cpf");
+		}
 
-        if (usuarioRepository.existsByEmailOrCpfAndIdNot(usuario.getEmail(), usuario.getCpf(), usuario.getId())) {
-            throw new EntityExistsException("Já existe um usuário com este email ou cpf");
-        }
+		if (!CPFValidator.validarCPF(usuario.getCpf())) {
+			throw new RuntimeException("CPF inválido");
+		}
 
-        if (!CPFValidator.validarCPF(usuario.getCpf())) {
-            throw new RuntimeException("CPF inválido");
-        }
+		usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
 
-        existente.setNome(usuario.getNome());
-        existente.setEmail(usuario.getEmail());
-        existente.setCpf(usuario.getCpf());
+		Cliente cliente = new Cliente();
+		cliente.setUsuario(usuario);
+		usuario.setCliente(cliente);
 
-        if (usuario.getSenha() != null && !usuario.getSenha().isBlank() &&
-                !passwordEncoder.matches(usuario.getSenha(), existente.getSenha())) {
-            existente.setSenha(passwordEncoder.encode(usuario.getSenha()));
-        }
+		return usuarioRepository.save(usuario);
+	}
 
-        return usuarioRepository.save(existente);
-    }
+	public Usuario editar(Usuario usuario) {
+		Usuario existente = usuarioRepository.findById(usuario.getId())
+				.orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
 
-    @Transactional
-    public void deletarUsuario(Integer id) {
-        if(usuarioLogado().getId().equals(id)) {
-            throw new RuntimeException("Você não pode se auto deletar!");
-        }
-        usuarioRepository.deleteById(id);
-    }
+		if (usuarioRepository.existsByEmailOrCpfAndIdNot(usuario.getEmail(), usuario.getCpf(), usuario.getId())) {
+			throw new EntityExistsException("Já existe um usuário com este email ou cpf");
+		}
+
+		if (!CPFValidator.validarCPF(usuario.getCpf())) {
+			throw new RuntimeException("CPF inválido");
+		}
+
+		existente.setNome(usuario.getNome());
+		existente.setEmail(usuario.getEmail());
+		existente.setCpf(usuario.getCpf());
+
+		if (usuario.getSenha() != null && !usuario.getSenha().isBlank() &&
+				!passwordEncoder.matches(usuario.getSenha(), existente.getSenha())) {
+			existente.setSenha(passwordEncoder.encode(usuario.getSenha()));
+		}
+
+		return usuarioRepository.save(existente);
+	}
+
+	@Transactional
+	public void deletarUsuario(Integer id) {
+		if (usuarioLogado().getId().equals(id)) {
+			throw new RuntimeException("Você não pode se auto deletar!");
+		}
+		usuarioRepository.deleteById(id);
+	}
 }
