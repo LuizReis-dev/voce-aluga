4452567b424026b68a0ed74a2778d714baf04e25
Luiz Reis <reisluiz05@gmail.com>
2025-07-10 19:26:39 -0300

    mostrando endereco da filial

--- src/main/java/com/cefet/vocealuga/repositories/FilialRepository.java
+++ src/main/java/com/cefet/vocealuga/repositories/FilialRepository.java
@@ -10,20 +10,28 @@ import java.util.List;
 
 public interface FilialRepository extends JpaRepository<Filial, Integer> {
 	@Query("""
-			SELECT DISTINCT new com.cefet.vocealuga.dtos.filial.FilialDTO(f.id, f.nome)
-			FROM Filial f
-			JOIN f.veiculos v
-			JOIN v.modelo mv
-			WHERE mv.id = :modeloId
-			  AND v.estadoVeiculo = com.cefet.vocealuga.models.EstadoVeiculo.DISPONIVEL
-			  AND f <> :filial
-			""")
+		SELECT DISTINCT new com.cefet.vocealuga.dtos.filial.FilialDTO(
+			f.id,
+			f.nome,
+			CONCAT(f.endereco.logradouro, ' ', f.endereco.numero, ' ', f.endereco.complemento)
+		)
+		FROM Filial f
+		JOIN f.veiculos v
+		JOIN v.modelo mv
+		WHERE mv.id = :modeloId
+		  AND v.estadoVeiculo = com.cefet.vocealuga.models.EstadoVeiculo.DISPONIVEL
+		  AND f <> :filial
+	""")
 	List<FilialDTO> buscarFiliaisComVeiculoDisponivelDoModeloExcetoFilial(
 			@Param("modeloId") Integer modeloId,
 			@Param("filial") Filial filial);
 
 	@Query("""
-			SELECT DISTINCT new com.cefet.vocealuga.dtos.filial.FilialDTO(f.id, f.nome)
+			SELECT DISTINCT new com.cefet.vocealuga.dtos.filial.FilialDTO(
+				f.id,
+				f.nome,
+				CONCAT(f.endereco.logradouro, ' ', f.endereco.numero, ' ', f.endereco.complemento)
+			)
 			FROM Filial f
 			JOIN f.veiculos v
 			JOIN v.modelo mv
